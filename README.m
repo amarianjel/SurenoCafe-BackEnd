123d8ef9ee43ce36e7f00c84ab7c500218e97dd9
amarianjel <abraham.marianjel@gmail.com>
2022-07-07 02:06:17 -0400

    Problemas con la conección a la base de datos, hay que tratar de hacerlo multi función

---
+++ README.m
@@ -0,0 +1,8 @@
+#Codigos
+npm init
+npm i nodemon --save
+npm,mysql
+npm i dotenv
+npm i cors 
+
+Los modelos son como estaran los registros en la base de datos
--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-//require('dotenv').config();
+require('dotenv').config();
 
 //Requiere
 var express = require('express');
+var cors = require('cors');
+const { dbConnection } = require('./database/config');
+//const mysql = require('mysql');
 
 // Crea el servidor
 var app = express();
 
-// !Escuchar peticiones
-/*app.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto http://localhost:' + process.env.PORT);
+// Configurar Cors
+app.use(cors());
+
+dbConnection();
+// configuración de la conexión
+/*const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sureno_cafe'
 });*/
-app.listen(3000, () => {
-    console.log('Servidor corriendo en puerto http://localhost:' + 3000);
+
+// conectar a la base de datos para
+//mc.connect();
+
+// Rutas
+// Recuperar todos los productos
+/*app.get('/dulces', function(req, res) {
+    mc.query('SELECT * FROM dulces', function(error, results, fields) {
+        if (error) throw error;
+        return res.send({
+            error: false,
+            data: results,
+            message: 'Lista de Dulces.'
+        });
+    });
+});*/
+
+app.get( '/', (req, res) => {
+    res.json({
+        ok:true,
+        msg:'Hola mundo'
+    })
+});
+
+// ? Escuchar peticiones
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto http://localhost:' + process.env.PORT);
 });
