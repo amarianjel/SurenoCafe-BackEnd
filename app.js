67800120e10b6c470270a9f5688d306947d09b24
Sebastián <sebastian.rocha1801@alumnos.ubiobio.cl>
2022-08-17 15:06:18 -0400

    :)

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 
 //Requiere
 var express = require('express');
+const mysql=require('mysql');
+var bcrypt= require('bcrypt');
 //var cors = require('cors');
 const bodyParser = require('body-parser'); //- Se usa para mandar atributos que no sean Json
 
 // Crea el servidor
 var app = express();
+
+const mc=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sureno_cafe'
+});
+
+mc.connect();
 
 // Configurar Cors
 app.use((req, res, next) => {
@@ -22,6 +33,32 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/cliente', function(req,res){
+    let datosCliente={
+        email:req.body.email,
+        nombre_cliente:req.body.name,
+        contrasenia:bcrypt.hashSync(req.body.password, 10),
+        telefono:parseInt(req.body.phone)
+        
+        
+    };
+    if(mc){
+        mc.query("INSERT INTO cliente SET ?", datosCliente, function(err, result){
+            if(err){
+                res.json({
+                    ok:false,
+                    mensaje: 'Error al crear al Cliente'
+                });
+            }else{
+                res.json({
+                    ok:true,
+                    mensaje:'Cliente creado correctamente'
+                });
+            }
+        });
+    }
+});
+
 // Routes
 app.use('/dulces', require('./routes/dulces-route'));
 app.use('/salados', require('./routes/salados-route'));
@@ -31,3 +68,4 @@ app.use('/auth', require('./routes/auth-route'));
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en puerto http://localhost:' + process.env.PORT);
 });
+
